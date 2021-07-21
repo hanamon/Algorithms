82a94f373d56c370d3906560b195677dc9fa522e
hanamon <devparkhana@gmail.com>
2021-07-21 22:01:42 +0900

    [ADD] divisorsFn

--- divisorsFn.js
+++ divisorsFn.js
@@ -1,7 +1,8 @@
-// 숫자를 입력 값는다.
-// 입력으로 받은 수의 약수를 배열로 리턴하는 함수를 만든다.
+// 요구 사항 : 입력으로 받은 수의 약수를 배열로 리턴하는 함수를 만든다.
+// 입력 : 숫자를 입력 받는다.
+// 출력 : 배열을 리턴한다.
 
-function divisorsFn(number) {
+const divisorsFn = (number) => {
   const result = [];
 
   for ( let i=1; i<=number; i++ ) {
@@ -11,4 +12,6 @@ function divisorsFn(number) {
   return result;
 }
 
-divisorsFn(4);
+// 입출력 예시
+divisorsFn(4); // [1, 2, 4]
+divisorsFn(7); // [1, 7]
