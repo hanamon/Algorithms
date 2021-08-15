12e96496874fd833030e52ecf5c7ad1f7b651bd5
hanamon <devparkhana@gmail.com>
2021-08-15 16:39:18 +0900

    [UPDATE] connectedVertices

--- data-structures/connectedVertices.js
+++ data-structures/connectedVertices.js
@@ -101,68 +101,140 @@ function dfs(matrix, vertex, visited) {
   });
 }
 
-/*--------------------------------------------------------------*/
-
-// [방법 2.] 인접리스트 이용 (BFS, DFS)
+// [방법 1-2] - 위와 동일한 방법인데 코드 디자인만 다름
 function connectedVertices(edges) {
-  // 간선에서 최대값을 추출한다.
-  const maxVertex = maxVertexFn(edges);
-  // 간선을 이용해 인접리스트를 만든다.
-  const adjList = createAdjList(edges, maxVertex);
-
-  // 방문한 정점을 담을 객체를 선언한다.
-  const visited = {};
-  // 컴포턴트를 카운트할 변수를 선언한다.
+  // 1. 간선을 이용해서 매트릭스를 만든다.
+  const matrix = createMatrix(edges);
+  // 2. 방문을 기록할 배열을 매트릭스 길이만큼 만든다.
+  const visited = new Array(matrix.length).fill(false);
+  // 3. 컴포넌트를 카운트할 변수를 생성한다.
   let count = 0;
-
-  // 그래프에 있는 버텍스를 전부 순회한다.
-  for( let vertex=0; vertex<=maxVertex; vertex++ ) {
-    if( !visited[vertex] ) {
-      bfs(adjList, vertex, visited);
+  // 4. 매트릭스를 순회한다.
+  matrix.forEach((row, idx) => {
+    if( !visited[idx] ) {
+      // 만약 현재 정점이 미확인이면 BFS를 실행한다.
+      // bfs(matrix, idx, visited);
+      dfs(matrix, idx, visited);
+      // 컴포넌트를 카운트 한다.
       count++;
     }
-  }
-  // 카운트를 리턴한다.
+  });
+  // 5. 카운트 변수를 리턴한다.
   return count;
 }
 
-function maxVertexFn(edges) {
-  return edges.reduce((a, c) => {
+function createMatrix(edges) {
+  // 간선에서 최대값을 추출한다.
+  // const max = Math.max(...edges.flat());
+  const max = edges.reduce((a, c) => {
     const bigger = Math.max(...c);
     if( a < bigger ) return bigger;
     return a;
   }, 0);
-}
-
-function createAdjList(edges, maxVertex) {
-  // 인접리스트는 객체를 이용한다.
-  const adjList = {};
-  
-  // 최대값 길이 만큼의 인접리스트를 만든다.
-  for( let i=0; i<=maxVertex; i++ ) {
-    adjList[i] = [];
-  }
-
-  // 간선을 순회하며 인접리스트에 표시한다.
-  edges.forEach((edges, idx) => {
-    const [row, col] = edges;
-    adjList[row].push(col);
-    adjList[col].push(row);
+  // 최대값 만큼의 매트릭스를 만든다.
+  const matrix = new Array(max + 1).fill(0).map(() => new Array(max + 1).fill(0));
+  // 간선을 순회하며 매트릭스에 간선을 표시한다.
+  edges.forEach((edge) => {
+    matrix[edge[0]][edge[1]] = 1;
+    matrix[edge[1]][edge[0]] = 1;
   });
-
-  return adjList;
+  // 매트릭스를 출력한다.
+  return matrix;
 }
 
-function bfs(adjList, vertex, visited) {
+function bfs(matrix, vertex, visited) {
+  // 자료구조 큐에 탐색할 정점을 쌓는다. 기본값은 vertex
   const queue = [vertex];
+  // 현재 정점을 true로 바꾼다.
   visited[vertex] = true;
+  // 쌓인 큐가 없어질 때까지 반복한다.
   while( queue.length ) {
     const current = queue.shift();
-    adjList[current].forEach((el) => {
-      if( !visited[el] ) {
-        queue.push(el);
-        visited[el] = true;
+    // 탐색할 정점을 순회하며 연결된 점들도 모두 visited 에 기록하고 그곳도 탐색할 수 있게 queue에 쌓는다.
+    matrix[current].forEach((el, idx) => {
+      if( el && !visited[idx] ) {
+        visited[idx] = true;
+        queue.push(idx);
       }
     });
   }
+}
+
+function dfs(matrix, vertex, visited) {
+  // 현재 탐색중인 정점을 기록한다.
+  visited[vertex] = true;
+  // 현재 정점을 순회한다.
+  matrix[vertex].forEach((el, idx) => {
+    // 연결된 정점이 심지어 미확인이라면 재귀를 호출한다.
+    if( el && !visited[idx] ) {
+      dfs(matrix, idx, visited);
+    } 
+  });
+}
+
+/*--------------------------------------------------------------*/
+
+// [방법 2.] 인접리스트 이용 (BFS, DFS)
+function connectedVertices(edges) {
+  // 인접 리스트로 구현
+  // 1. 간선의 최대값을 구한다.
+  const max = Math.max(...edges.flat());
+
+  // 2. 최대값 만큼의 인접 리스트를 만든다.
+  const abjList = {};
+  for( let i=0; i<=max; i++ ) {
+    abjList[i] = [];
+  }
+
+  // 3. 간선을 순회하며 인접 리스트에 표시한다.
+  edges.forEach((edge) => {
+    const [row, col] = edge;
+    abjList[row].push(col);
+    abjList[col].push(row);
+  });
+
+  // 4. 탐색 기록 객체를 선언한다.
+  const visited = {};
+
+  // 5. 컴포넌트 카운트 변수를 선언한다.
+  let count = 0;
+
+  // 6. 간선의 최대값 만큼 순회하며 방문하지 않은 정점을 방문하고 마지막에 카운트한다.
+  for( let i=0; i<=max; i++ ) {
+    if( !visited[i] ) {
+      dfs(abjList, i, visited);
+      count++;
+    }
+  }
+
+  // 7. 카운트 변수를 리턴한다.
+  return count;
+}
+
+function bfs(abjList, vertex, visited) {
+  // 큐의 자료구조에 탐색할 정점을 쌓는다. 기본은 vertex
+  const queue = [vertex];
+  // 현재 탐색중인 정점을 기록한다.
+  visited[vertex] = true;
+  // 탐색할 정점이 있으면 아래 코드를 실행한다.
+  while( queue.length ) {
+    const current = queue.shift();
+    // 현재 탐색중인 정점을 순회하며 연결된 지점이 있는지 확인한다.
+    abjList[current].forEach((el) => {
+      // 주의 : index 가 아니라 el 로 확인해야한다.
+      if( !visited[el] ) {
+        visited[el] = true;
+        queue.push(el);
+      }
+    });
+  }
+}
+
+function dfs(abjList, vertex, visited) {
+  visited[vertex] = true;
+  abjList[vertex].forEach((el) => {
+    if( !visited[el] ) {
+      dfs(abjList, el, visited);
+    }
+  });
 }
