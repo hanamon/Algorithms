eab3467bd58a5f636006d1a54903792eaa063afa
hanamon <devparkhana@gmail.com>
2021-08-17 13:42:59 +0900

    [UPDATE] code

--- toy-algorithms/bubbleSort.js
+++ toy-algorithms/bubbleSort.js
@@ -68,7 +68,7 @@ const bubbleSort = function (arr) {
       // 매 반복마다 i번째로 큰 수가 마지막에서 i번째 위치하게 된다.
       // 이미 정렬된 요소는 고려할 필요가 없으므로 'j<arr.length-1-i'까지만 비교한다.
       if( arr[j] > arr[j+1] ) {
-        swap_1(j, j+1, arr);
+        swap(j, j+1, arr);
       }
     }
   }
@@ -101,7 +101,7 @@ const bubbleSort = function (arr) {
     for( let j=0; j<arr.length-1-i; j++ ) {
       if( arr[j] > arr[j+1] ) {
         swaps++;
-        swap_2(j, j + 1, arr);
+        swap(j, j + 1, arr);
       }
     }
     // 어떤 요소도 swap되지 않은 경우, 배열은 정렬된 상태이다.
--- uncategorized/fibonacciFn.js
+++ uncategorized/fibonacciFn.js
@@ -10,14 +10,15 @@
 /*--------------------------------------------------------------*/
 
 // [방법 1.] - Recursion + Memoization (재귀 + 메모이제이션) - Top-Down 방식
-const fibonacciFn = function (number, memo = []) {
+const fibonacci = function (number, memo = []) {
   // 이미 해결한 하위 문제인지 찾아본다. (두 번 계산 안함)
   if( memo[number] !== undefined ) return memo[number];
   // 재귀 탈출 조건
+  if( number === 0 ) return 0;
   if( number <= 2 ) return 1;
 
   // 메모에 없다면 재귀로 결괏값을 도출하여 res 에 할당
-  const result = fibonacciFn(number-1, memo) + fibonacciFn(number-2, memo);
+  const result = fibonacci(number-1, memo) + fibonacci(number-2, memo);
   // 추후 동일한 문제를 만났을 때 사용하기 위해 리턴 전에 memo 에 저장한다.
   memo[number] = result;
 
